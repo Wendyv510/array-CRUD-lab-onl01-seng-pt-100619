5843163d73e883939994507b6813c2fe7fb62963
Wendy Harris <wendyv510@hotmail.com>
2019-10-13 19:07:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,12 +27,15 @@ end
 
 def retrieve_element_from_index(array, index_number)
 my_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+my_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  my_array=["wow", "I", "am", "really", "learning", "arrays!"]
+  my_array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  my_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  my_array[-1]
 end
